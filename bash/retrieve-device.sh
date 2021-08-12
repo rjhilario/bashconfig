5d653bed34fc884c36b664855e7386ea50cdbc04
RJ Hilario <rodjonh@gmail.com>
2021-08-12 18:42:20 +0800

    Edited retrieve device script

--- bash/retrieve-device.sh
+++ bash/retrieve-device.sh
@@ -1,1 +1,1 @@
-curl -X GET --header 'Accept: application/json' --header "X-Auth-Token: $mytoken" 'https://api.equinix.com/metal/v1/devices/{id}'
+curl -X GET --header 'Accept: application/json' --header "X-Auth-Token: $mytoken" 'https://api.equinix.com/metal/v1/devices/218b6669-1c04-43cb-9073-f1476abd8239'

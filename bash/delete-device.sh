0830fdd5f596773cabb53cc0adc37dd2955e2315
RJ Hilario <rodjonh@gmail.com>
2021-08-12 21:14:55 +0800

    Make script executable

--- bash/delete-device.sh
+++ bash/delete-device.sh
@@ -1,1 +1,1 @@
-curl -X DELETE -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" 'https://api.equinix.com/metal/v1/devices/{id}'
+curl -X DELETE -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" 'https://api.equinix.com/metal/v1/devices/218b6669-1c04-43cb-9073-f1476abd8239'

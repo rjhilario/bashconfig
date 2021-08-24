793dea86a5f0a21ffeae3b054a2d47d27e07de26
RJ Hilario <rodjonh@gmail.com>
2021-08-24 16:52:40 +0800

    Edited to SG1 facility and use n2.xlarge.x86 server type

--- bash/deploy-server.sh
+++ bash/deploy-server.sh
@@ -1,8 +1,8 @@
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" 'https://api.equinix.com/metal/v1/projects/7ab7c2b6-86b1-4fbb-b9cc-cfe11a2f7cf8/devices' -d \
     '{
-        "facility": "sv15",
-        "plan": "s3.xlarge.x86",
-        "hostname": "rj-test-dcui-console",
+        "facility": "sg1",
+        "plan": "n2.xlarge.x86",
+        "hostname": "rj-esxi70-test",
         "description": "RJ Test Deploy",
         "operating_system": "vmware_esxi_7_0",
         "tags": ["shell script deploy"],

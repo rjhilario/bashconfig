0253ddf000de9a607e48bab75b517a0add4561c9
RJ Hilario <rodjonh@gmail.com>
2021-08-27 22:06:47 +0800

    Removed non-working scripts

--- bash/deploy02.sh
+++
@@ -1,9 +0,0 @@
-curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" 'https://api.equinix.com/metal/v1/projects/7ab7c2b6-86b1-4fbb-b9cc-cfe11a2f7cf8/devices' -d \
-    '{
-        "facility": "sg1",
-        "plan": "n2.xlarge.x86",
-        "hostname": "rj-enc-test",
-        "description": "RJ Test Deploy",
-        "operating_system": "windows_2019",
-        "tags": ["shell script deploy"]
-        }'
--- bash/deploy03.sh
+++
@@ -1,9 +0,0 @@
-curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" 'https://api.equinix.com/metal/v1/projects/7ab7c2b6-86b1-4fbb-b9cc-cfe11a2f7cf8/devices' -d \
-    '{
-        "facility": "ewr1",
-        "plan": "c3.small.x86",
-        "hostname": "rj-c3-test",
-        "description": "RJ Test Deploy",
-        "operating_system": "ubuntu_20_04",
-        "tags": ["shell script deploy"]
-        }'

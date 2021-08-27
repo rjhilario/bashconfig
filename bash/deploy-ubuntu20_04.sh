11eb2705a8a773d36b43962bdfbd3975887bc83e
RJ Hilario <rodjonh@gmail.com>
2021-08-28 02:29:24 +0800

    Added script for Ubuntu 20.04 deploy

---
+++ bash/deploy-ubuntu20_04.sh
@@ -0,0 +1,9 @@
+curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" 'https://api.equinix.com/metal/v1/projects/7ab7c2b6-86b1-4fbb-b9cc-cfe11a2f7cf8/devices' -d \
+    '{
+        "facility": "sg1",
+        "plan": "n2.xlarge.x86",
+        "hostname": "rj-ub-test",
+        "description": "RJ Test Deploy",
+        "operating_system": "ubuntu_20_04",
+        "tags": ["shell script deploy"]
+        }'

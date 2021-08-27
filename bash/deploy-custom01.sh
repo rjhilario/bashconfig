4a547228ad8df683eae73efc3b8e80ebeef6e337
RJ Hilario <rodjonh@gmail.com>
2021-08-28 07:44:19 +0800

    Edited hostname

--- bash/deploy-custom01.sh
+++ bash/deploy-custom01.sh
@@ -2,7 +2,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "ht
     '{
         "facility": "sg1",
         "plan": "n2.xlarge.x86",
-        "hostname": "rj-custom-test",
+        "hostname": "rj-custom-ipxe-test",
         "description": "RJ Test Deploy",
         "operating_system": "custom_ipxe",
         "always_pxe": true,

00025d8ae0d8c87633ab261892eeaaf5788a0073
RJ Hilario <rodjonh@gmail.com>
2021-08-28 06:45:20 +0800

    Added deploy script for custom iPXE, base format

---
+++ bash/deploy-custom01.sh
@@ -0,0 +1,11 @@
+curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
+    '{
+        "facility": "sg1",
+        "plan": "n2.xlarge.x86",
+        "hostname": "rj-custom-test",
+        "description": "RJ Test Deploy",
+        "operating_system": "ubuntu_20_04",
+        "always_pxe": true,
+        "ipxe_script_url": "https://boot.netboot.xyz/"
+        "tags": ["shell script deploy"]
+        }'

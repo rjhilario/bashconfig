f43debb7a48a2883ec42d4a542483a2de3fb5f3a
RJ Hilario <rodjonh@gmail.com>
2021-08-31 02:43:16 +0800

    Added deploy script for ESXi 7.0

---
+++ bash/deploy-esxi70.sh
@@ -0,0 +1,16 @@
+curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" 'https://api.equinix.com/metal/v1/projects/7ab7c2b6-86b1-4fbb-b9cc-cfe11a2f7cf8/devices' -d \
+    '{
+        "facility": "sg1",
+        "plan": "n2.xlarge.x86",
+        "hostname": "rj-esxi70-test",
+        "description": "RJ Test Deploy",
+        "operating_system": "vmware_esxi_7_0",
+        "tags": ["shell script deploy"],
+        "public_ipv4_subnet_size": "28",
+        "ip_adresses": 
+        [
+        { "address_family": 4, "public": false },
+        { "address_family": 4, "public": true, "cidr": 28, "ip_reservations":
+        ["88117484-e3f2-4434-8172-82b99a9d1324"] }
+        ]
+        }'

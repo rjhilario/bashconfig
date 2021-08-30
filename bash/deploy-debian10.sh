cc2c860d17a80a35df55a86e74beafe2650c80d8
RJ Hilario <rodjonh@gmail.com>
2021-08-30 21:17:42 +0800

    Added new deploy script for Debian 10

---
+++ bash/deploy-debian10.sh
@@ -0,0 +1,9 @@
+curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
+    '{
+        "facility": "sg1",
+        "plan": "n2.xlarge.x86",
+        "hostname": "rj-ub-test",
+        "description": "RJ Test Deploy",
+        "operating_system": "ubuntu_20_04",
+        "tags": ["shell script deploy"]
+        }'

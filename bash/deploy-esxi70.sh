c9c1d7af0e1c5d494b56ed1f74b347e52289b287
RJ Hilario <rodjonh@gmail.com>
2021-08-31 02:45:08 +0800

    Removed portion for reserved IP block

--- bash/deploy-esxi70.sh
+++ bash/deploy-esxi70.sh
@@ -5,12 +5,5 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" 'ht
         "hostname": "rj-esxi70-test",
         "description": "RJ Test Deploy",
         "operating_system": "vmware_esxi_7_0",
-        "tags": ["shell script deploy"],
-        "public_ipv4_subnet_size": "28",
-        "ip_adresses": 
-        [
-        { "address_family": 4, "public": false },
-        { "address_family": 4, "public": true, "cidr": 28, "ip_reservations":
-        ["88117484-e3f2-4434-8172-82b99a9d1324"] }
-        ]
+        "tags": ["shell script deploy"]
         }'

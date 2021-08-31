ebd0249e101e0c554292df865c5967cb04f710af
RJ Hilario <rodjonh@gmail.com>
2021-08-31 23:42:21 +0800

    Edited project ID to be a variable

--- bash/deploy02.sh
+++ bash/deploy02.sh
@@ -1,4 +1,4 @@
-curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" 'https://api.equinix.com/metal/v1/projects/7ab7c2b6-86b1-4fbb-b9cc-cfe11a2f7cf8/devices' -d \
+curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
         "facility": "sg5",
         "plan": "n2.xlarge.x86",

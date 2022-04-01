58c519ea62798c6b2b0d11059c80d5c32c06cc06
RJ Hilario <rodjonh@gmail.com>
2022-04-01 22:41:23 +0800

    Added jq to format output

--- bash/get-usages.sh
+++ bash/get-usages.sh
@@ -1,1 +1,1 @@
-curl -X GET --header 'Accept: application/json' --header "X-Auth-Token: $mytoken" 'https://api.equinix.com/metal/v1/projects/7ab7c2b6-86b1-4fbb-b9cc-cfe11a2f7cf8/usages'
+curl -X GET -H "Content-Type: application/json" -H "X-Consumer-Token: "$ctoken"" -H "X-Auth-Token: $mytoken" -H "X-Packet-Staff: true" "https://api.equinix.com/metal/v1/projects/7ab7c2b6-86b1-4fbb-b9cc-cfe11a2f7cf8/usages" | jq

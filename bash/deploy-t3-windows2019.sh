2e2c0bd1e16ae9efb6ab88725bf689e8d289b73c
RJ Hilario <rodjonh@gmail.com>
2022-08-31 16:20:20 +0800

    Added another script for Windows 2019 on t3.small.x86

---
+++ bash/deploy-t3-windows2019.sh
@@ -0,0 +1,9 @@
+curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
+    '{
+        "facility": "da6",
+        "plan": "t3.small.x86",
+        "hostname": "rj-da-dc01",
+        "description": "RJ Domain Controller",
+        "operating_system": "windows_2019",
+        "tags": ["shell script deploy"]
+        }'

38bafe216183193bc7ea11bbd0fdd1dda1ee03c5
RJ Hilario <rodjonh@gmail.com>
2022-11-24 21:21:49 +0800

    Changed ID to variable

--- bash/delete_spot.sh
+++ bash/delete_spot.sh
@@ -1,1 +1,1 @@
-curl -X GET -H "Content-Type: application/json" -H "X-Auth-Token: $mytoken" -H "https://api.equinix.com/metal/v1/spot-market-requests/6ac7ecc8-7f63-4317-9a2e-6420e13fc0bc"
+curl -X DELETE -H "Content-Type: application/json" -H "X-Auth-Token: $mytoken" 'https://api.equinix.com/metal/v1/spot-market-requests/{id}'

58c7b9039df9a47a6167c0dad91acfad8de624a5
RJ Hilario <rodjonh@gmail.com>
2022-11-29 15:34:35 +0800

    Corrected variable name

--- bash/deploy-server.sh
+++ bash/deploy-server.sh
@@ -1,4 +1,4 @@
-curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" 'https://api.equinix.com/metal/v1/projects/${id}/devices' -d \
+curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" 'https://api.equinix.com/metal/v1/projects/${project_id}/devices' -d \
     '{
         "facility": "sg1",
         "plan": "n2.xlarge.x86",

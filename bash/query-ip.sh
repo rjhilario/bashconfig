04a5325b75d463a665e21162cf237bcdb399a0b5
RJ Hilario <rodjonh@gmail.com>
2022-11-30 12:26:06 +0800

    Changed project ID to a variable

--- bash/query-ip.sh
+++ bash/query-ip.sh
@@ -1,1 +1,1 @@
-curl -X GET --header 'Accept: application/json' --header "X-Auth-Token: "$mytoken"" 'https://api.equinix.com/metal/v1/projects/7ab7c2b6-86b1-4fbb-b9cc-cfe11a2f7cf8/ips'
+curl -X GET --header 'Accept: application/json' --header "X-Auth-Token: "$mytoken"" 'https://api.equinix.com/metal/v1/projects/${project_id}/ips'

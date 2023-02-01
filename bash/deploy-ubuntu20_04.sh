be2f41268041ca800b2852e669cbdded421e1e4c
RJ Hilario <rodjonh@gmail.com>
2023-02-01 20:13:07 +0800

    Edited values, hostname

--- bash/deploy-ubuntu20_04.sh
+++ bash/deploy-ubuntu20_04.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
-        "facility": "sg1",
-        "plan": "n2.xlarge.x86",
-        "hostname": "rj-ub-test",
-        "description": "RJ Test Deploy",
+        "facility": "da6",
+        "plan": "t3.small.x86",
+        "hostname": "rj-da-kubernetes-test",
+        "description": "RJ Kubernetes test node",
         "operating_system": "ubuntu_20_04",
         "tags": ["shell script deploy"]
         }'

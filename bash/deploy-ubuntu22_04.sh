fc86aec603d58fcd17ccc49c59885ef64317565c
RJ Hilario <rodjonh@gmail.com>
2023-02-02 23:19:33 +0800

    Edited hostname

--- bash/deploy-ubuntu22_04.sh
+++ bash/deploy-ubuntu22_04.sh
@@ -3,7 +3,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "ht
     '{
         "facility": "da6",
         "plan": "t3.small.x86",
-        "hostname": "rj-da-kubernetes-test",
+        "hostname": "rj-da-ubuntu22",
         "description": "RJ Kubernetes test node",
         "operating_system": "ubuntu_22_04",
         "tags": ["shell script deploy"]

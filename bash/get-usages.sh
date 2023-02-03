74f4147fec7fa2307973c8ed2ce0112df13fd1dc
RJ Hilario <rodjonh@gmail.com>
2023-02-03 22:01:50 +0800

    Added shebang

--- bash/get-usages.sh
+++ bash/get-usages.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 curl -X GET -H "Content-Type: application/json" -H "X-Consumer-Token: "$ctoken"" -H "X-Auth-Token: $mytoken" -H "X-Packet-Staff: true" "https://api.equinix.com/metal/v1/projects/${project_id}/usages" | jq

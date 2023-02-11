f776e06bf56b5d31501be783b7e63e1245e68e6f
RJ Hilario <rodjonh@gmail.com>
2023-02-11 18:27:21 +0800

    Added shebang

--- bash/query-reservation.sh
+++ bash/query-reservation.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 curl -X GET -H "Content-Type: application/json" -H "X-Consumer-Token: "$ctoken"" -H "X-Auth-Token: "$mytoken"" -H "X-Packet-Staff: true" "https://api.equinix.com/metal/v1/hardware-reservations/${reservation_id}?include=server"

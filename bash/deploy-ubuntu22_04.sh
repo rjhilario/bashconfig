b45853e2e2661ed61a4482ec995b4cd52a5eb4f1
RJ Hilario <rodjonh@gmail.com>
2023-02-15 18:48:36 +0800

    Modified hostname

--- bash/deploy-ubuntu22_04.sh
+++ bash/deploy-ubuntu22_04.sh
@@ -3,7 +3,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "ht
     '{
         "facility": "da6",
         "plan": "t3.small.x86",
-        "hostname": "rj-da-ubuntu22",
+        "hostname": "rj-da-ubuntu22-test01",
         "description": "RJ Ubuntu 22 Test Machine",
         "operating_system": "ubuntu_22_04",
         "tags": ["shell script deploy"]

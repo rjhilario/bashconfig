0db9af3b7be71462186a4c62578c2d1f73815202
RJ Hilario <rodjonh@gmail.com>
2023-02-16 00:14:40 +0800

    Edited description

--- bash/deploy-ubuntu22_04.sh
+++ bash/deploy-ubuntu22_04.sh
@@ -4,7 +4,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "ht
         "facility": "da6",
         "plan": "t3.small.x86",
         "hostname": "rj-da-ubuntu22-test01",
-        "description": "RJ Ubuntu 22 Test Machine",
+        "description": "RJ Ubuntu 22 Test",
         "operating_system": "ubuntu_22_04",
         "tags": ["shell script deploy"]
         }'

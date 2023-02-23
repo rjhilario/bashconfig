2b391e405a2b5e6b74fc8a80b5e632cc4adb2f3a
RJ Hilario <rodjonh@gmail.com>
2023-02-23 22:55:20 +0800

    Added shebang

--- bash/deploy-win2016.sh
+++ bash/deploy-win2016.sh
@@ -1,3 +1,4 @@
+#!/usr/bin
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
         "facility": "sg5",

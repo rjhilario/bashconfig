af5f57eab03af87b49bc7720754efbabaad2e713
RJ Hilario <rodjonh@gmail.com>
2023-02-25 22:14:03 +0800

    Edited hostname

--- bash/deploy-win2016.sh
+++ bash/deploy-win2016.sh
@@ -3,7 +3,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "ht
     '{
         "facility": "da6",
         "plan": "t3.small.x86",
-        "hostname": "rj-win2k16-test",
+        "hostname": "rj-da-windows-test",
         "description": "RJ Test Deploy",
         "operating_system": "windows_2016",
         "tags": ["shell script deploy"]

9848da530bd2ad4bdf5353411aee586e3a54904c
RJ Hilario <rodjonh@gmail.com>
2023-02-25 22:12:41 +0800

    Edited facility

--- bash/deploy-win2016.sh
+++ bash/deploy-win2016.sh
@@ -1,7 +1,7 @@
 #!/usr/bin
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
-        "facility": "sg5",
+        "facility": "da6",
         "plan": "n2.xlarge.x86",
         "hostname": "rj-win2k16-test",
         "description": "RJ Test Deploy",

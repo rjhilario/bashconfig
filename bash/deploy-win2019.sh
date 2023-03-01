5576374442cbdd9d4354c311d84ff23a3ec946d2
RJ Hilario <rodjonh@gmail.com>
2023-03-01 20:23:17 +0800

    Changed server plan to t3.small.x86

--- bash/deploy-win2019.sh
+++ bash/deploy-win2019.sh
@@ -2,7 +2,7 @@
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
         "facility": "da6",
-        "plan": "n2.xlarge.x86",
+        "plan": "t3.small.x86",
         "hostname": "rj-win2k19-test",
         "description": "RJ Test Deploy",
         "operating_system": "windows_2019",

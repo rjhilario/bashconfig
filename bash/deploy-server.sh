84e5e8a9e0a2ed4a390627450edb5aa674badace
RJ Hilario <rodjonh@gmail.com>
2023-03-10 23:44:06 +0800

    Deleted unnecessary lines

--- bash/deploy-server.sh
+++ bash/deploy-server.sh
@@ -7,11 +7,4 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" 'ht
         "description": "RJ Test Deploy t3.small.x86",
         "operating_system": "ubuntu_22_04",
         "tags": ["shell script deploy"],
-        "public_ipv4_subnet_size": "28",
-        "ip_adresses": 
-        [
-        { "address_family": 4, "public": false },
-        { "address_family": 4, "public": true, "cidr": 28, "ip_reservations":
-        ["88117484-e3f2-4434-8172-82b99a9d1324"] }
-        ]
         }'

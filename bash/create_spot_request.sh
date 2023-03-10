889570a667f27ad12f1d930cb5ff37e49681f27a
RJ Hilario <rodjonh@gmail.com>
2023-03-10 23:45:45 +0800

    Added shebang

--- bash/create_spot_request.sh
+++ bash/create_spot_request.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/spot-market-requests" -d \
     '{
         "devices_max": 1,

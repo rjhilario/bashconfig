859f34da65404ad0c993736db940c405220a1057
RJ Hilario <rodjonh@gmail.com>
2023-03-15 16:34:37 +0800

    Edited hostname

--- bash/deploy-debian10.sh
+++ bash/deploy-debian10.sh
@@ -3,7 +3,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "ht
     '{
         "facility": "da6",
         "plan": "t3.small.x86",
-        "hostname": "rj-deb10-test",
+        "hostname": "rj-da-debian10-test",
         "description": "RJ Test Deploy",
         "operating_system": "debian_10",
         "tags": ["shell script deploy"]

0add9de49a32dd75212c356de210e0bca6a65798
RJ Hilario <rodjonh@gmail.com>
2023-03-15 16:35:07 +0800

    Edited description

--- bash/deploy-debian10.sh
+++ bash/deploy-debian10.sh
@@ -4,7 +4,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "ht
         "facility": "da6",
         "plan": "t3.small.x86",
         "hostname": "rj-da-debian10-test",
-        "description": "RJ Test Deploy",
+        "description": "RJ Debian Test",
         "operating_system": "debian_10",
         "tags": ["shell script deploy"]
         }'

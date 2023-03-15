a1cc0476b4d9d06f5b43725263462345006bce52
RJ Hilario <rodjonh@gmail.com>
2023-03-15 16:32:42 +0800

    Added shebang

--- bash/deploy-debian10.sh
+++ bash/deploy-debian10.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
         "facility": "ewr1",

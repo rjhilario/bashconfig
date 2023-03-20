d8595c786ac6bd9443a7fb86c3e5897bf963d2c5
RJ Hilario <rodjonh@gmail.com>
2023-03-20 22:00:27 +0800

    Changed server plan

--- bash/deploy-win2016.sh
+++ bash/deploy-win2016.sh
@@ -2,7 +2,7 @@
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
         "facility": "da6",
-        "plan": "t3.small.x86",
+        "plan": "c3.small.x86",
         "hostname": "rj-da-windows-test",
         "description": "RJ Windows Test Deploy",
         "operating_system": "windows_2016",

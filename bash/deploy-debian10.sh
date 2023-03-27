6277e801cf7df4416b8a162f7990673bc024f6a6
RJ Hilario <rodjonh@gmail.com>
2023-03-27 23:48:18 +0800

    Edited plan

--- bash/deploy-debian10.sh
+++ bash/deploy-debian10.sh
@@ -2,7 +2,7 @@
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
         "facility": "da6",
-        "plan": "t3.small.x86",
+        "plan": "c3.small.x86",
         "hostname": "rj-da-debian10-test",
         "description": "RJ Debian 10 Test",
         "operating_system": "debian_10",

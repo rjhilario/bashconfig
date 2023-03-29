1817011e9e5613f606fb01acb01ffb33707948ed
RJ Hilario <rodjonh@gmail.com>
2023-03-29 13:15:41 +0800

    Changed server config to t3.small.x86

--- bash/deploy-custom01.sh
+++ bash/deploy-custom01.sh
@@ -2,7 +2,7 @@
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
         "facility": "sg1",
-        "plan": "n2.xlarge.x86",
+        "plan": "t3.large.x86",
         "hostname": "rj-custom-ipxe-test01",
         "description": "RJ Test Deploy",
         "operating_system": "custom_ipxe",

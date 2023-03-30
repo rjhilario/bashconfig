b1d4bf5b4f9705001b356ae5fccf9c552fd8e9ae
RJ Hilario <rodjonh@gmail.com>
2023-03-30 22:34:28 +0800

    Edited hostname

--- bash/deploy-win2019.sh
+++ bash/deploy-win2019.sh
@@ -3,7 +3,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "ht
     '{
         "facility": "da6",
         "plan": "t3.small.x86",
-        "hostname": "rj-da-win2019-test01",
+        "hostname": "rj-da-win2019-test",
         "description": "RJ Windows 2019 Test Deploy",
         "operating_system": "windows_2019",
         "tags": ["shell script deploy"]

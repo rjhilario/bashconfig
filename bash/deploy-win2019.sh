961efe0fd79a0cec2154a8589ceb3551b2d563ca
RJ Hilario <rodjonh@gmail.com>
2023-03-31 16:04:19 +0800

    Edited server config to c3.small.x86

--- bash/deploy-win2019.sh
+++ bash/deploy-win2019.sh
@@ -2,7 +2,7 @@
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
         "facility": "da6",
-        "plan": "t3.small.x86",
+        "plan": "c3.small.x86",
         "hostname": "rj-da-win2019-test",
         "description": "RJ Windows 2019 Test Deploy",
         "operating_system": "windows_2019",

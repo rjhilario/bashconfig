38e5c3a6b40912262a82a6b54d0b88a86d2009fc
RJ Hilario <rodjonh@gmail.com>
2023-04-12 16:34:54 +0800

    Edited description

--- bash/deploy-server.sh
+++ bash/deploy-server.sh
@@ -4,7 +4,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" 'ht
         "facility": "da6",
         "plan": "m3.small.x86",
         "hostname": "rj-da-test01",
-        "description": "RJ Test Deploy c3.small.x86",
+        "description": "RJ Test Deploy m3.small.x86",
         "operating_system": "ubuntu_22_04",
         "tags": ["shell script deploy"],
         }'

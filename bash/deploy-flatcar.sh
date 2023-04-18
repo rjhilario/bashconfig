3065b1f64de784d5fe0e2d21f6068d7e4fa7a6c0
RJ Hilario <rodjonh@gmail.com>
2023-04-18 19:56:00 +0800

    Added deployment script for flatcar

---
+++ bash/deploy-flatcar.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
+    '{
+        "facility": "da6",
+        "plan": "m3.small.x86",
+        "hostname": "rj-da-flatcar-test",
+        "description": "RJ Dallas deploy Debian 11",
+        "operating_system": "debian_11",
+        "tags": ["shell script deploy"]
+        }'

a579e5ae82ed6f0623c8b0b93dce07ed117617dc
RJ Hilario <rodjonh@gmail.com>
2023-04-22 18:45:32 +0800

    Changed hostname

--- bash/deploy-server.sh
+++ bash/deploy-server.sh
@@ -3,7 +3,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" 'ht
     '{
         "facility": "da6",
         "plan": "m3.small.x86",
-        "hostname": "rj-da-test01",
+        "hostname": "rj-da-m3-small-test",
         "description": "RJ Test Deploy m3.small.x86",
         "operating_system": "ubuntu_22_04",
         "tags": ["shell script deploy"],

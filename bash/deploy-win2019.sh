8280bc328b9ba60ef36735e40c98081667c13705
RJ Hilario <rodjonh@gmail.com>
2023-04-22 16:56:14 +0800

    Edited data

--- bash/deploy-win2019.sh
+++ bash/deploy-win2019.sh
@@ -4,7 +4,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "ht
         "facility": "da6",
         "plan": "c3.small.x86",
         "hostname": "rj-da-win2019",
-        "description": "RJ Windows 2019 Test Deploy",
+        "description": "RJ Windows 2019 Test",
         "operating_system": "windows_2019",
         "tags": ["shell script deploy"]
         }'

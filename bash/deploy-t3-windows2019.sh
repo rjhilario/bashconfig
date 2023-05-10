ef7e08f5615b028f565a1a502d680955f4a055e6
RJ Hilario <rodjonh@gmail.com>
2023-05-10 23:46:58 +0800

    Edited description

--- bash/deploy-t3-windows2019.sh
+++ bash/deploy-t3-windows2019.sh
@@ -4,7 +4,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "ht
         "facility": "da6",
         "plan": "m3.small.x86",
         "hostname": "rj-da-dc01",
-        "description": "RJ Test Deploy Windows DC",
+        "description": "RJ Test Deploy Windows 2019 Test",
         "operating_system": "windows_2019_standard",
         "tags": ["shell script deploy"]
         }'

8b144dac3fc09c5527a582dc3c4c1691249c1d9d
RJ Hilario <rodjonh@gmail.com>
2023-05-12 13:43:41 +0800

    Edited description

--- bash/deploy-t3-ubuntu20.sh
+++ bash/deploy-t3-ubuntu20.sh
@@ -3,7 +3,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" 'ht
     '{
         "facility": "da6",
         "plan": "t3.small.x86",
-        "hostname": "rj-da-t3-ubuntu20",
+        "hostname": "rj-da-t3-testing",
         "description": "RJ Dallas Deployment Ubuntu 20.04 t3.small.x86",
         "operating_system": "ubuntu_20_04",
         "tags": ["shell script deploy"]

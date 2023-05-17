97787cb91a761480e078254132826038e4240dae
RJ Hilario <rodjonh@gmail.com>
2023-05-17 22:51:35 +0800

    Edited description

--- bash/deploy-t3_small.sh
+++ bash/deploy-t3_small.sh
@@ -4,7 +4,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" 'ht
         "facility": "da6",
         "plan": "t3.small.x86",
         "hostname": "rj-da-t3-test01",
-        "description": "RJ Test Deploy",
+        "description": "RJ t3.small.x86 test deploy",
         "operating_system": "ubuntu_22_04",
         "tags": ["shell script deploy"],
         }'

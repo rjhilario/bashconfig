237cc84afe6558f72627cc42b730ccfe52cfd4ec
RJ Hilario <rodjonh@gmail.com>
2023-05-25 23:32:02 +0800

    Changed server to c3.medium.x86

--- bash/deploy-rhel8.sh
+++ bash/deploy-rhel8.sh
@@ -2,7 +2,7 @@
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
         "facility": "da6",
-        "plan": "n3.xlarge.x86",
+        "plan": "c3.medium.x86",
         "hostname": "rj-da-rheltest",
         "description": "RJ Test Deploy RHEL8",
         "operating_system": "rhel_8",

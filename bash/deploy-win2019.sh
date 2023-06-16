2c062a715aefa9e150b438f041f61a2bc321a800
RJ Hilario <rodjonh@gmail.com>
2023-06-16 23:06:12 +0800

    Changed server to c3.medium.x86

--- bash/deploy-win2019.sh
+++ bash/deploy-win2019.sh
@@ -2,7 +2,7 @@
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
         "facility": "sv15",
-        "plan": "c3.small.x86",
+        "plan": "c3.medium.x86",
         "hostname": "rj-da-win2019",
         "description": "RJ Windows 2019 Test",
         "operating_system": "windows_2019",

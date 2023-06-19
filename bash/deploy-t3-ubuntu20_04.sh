dad36a83c390d63087d51305f5d28e7e80d832e1
RJ Hilario <rodjonh@gmail.com>
2023-06-19 20:39:53 +0800

    Changed server plan to c3.large.x86

--- bash/deploy-t3-ubuntu20_04.sh
+++ bash/deploy-t3-ubuntu20_04.sh
@@ -2,9 +2,9 @@
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
         "facility": "da6",
-        "plan": "m3.small.x86",
-        "hostname": "rj-da-ubuntu-test",
-        "description": "RJ Kubernetes test node",
+        "plan": "t3.small.x86",
+        "hostname": "rj-da-proxytest",
+        "description": "RJ-proxytest",
         "operating_system": "ubuntu_20_04",
         "tags": ["shell script deploy"]
         }'

b0fc2d6bf5c41eb3dc0af795f4cf9b4a9fd20367
RJ Hilario <rodjonh@gmail.com>
2023-06-21 22:37:22 +0800

    Changed facility to FR8

--- bash/deploy-server.sh
+++ bash/deploy-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" 'https://api.equinix.com/metal/v1/projects/${project_id}/devices' -d \
     '{
-        "facility": "da6",
+        "facility": "fr8",
         "plan": "n3.xlarge.x86",
         "hostname": "rj-da-n3.xlarge.test",
         "description": "RJ Test Deploy m3.small.x86",

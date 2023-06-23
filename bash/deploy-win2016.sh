e98375356c2200759db809b219df70599244b140
RJ Hilario <rodjonh@gmail.com>
2023-06-23 22:37:05 +0800

    Changed facility to SG4

--- bash/deploy-win2016.sh
+++ bash/deploy-win2016.sh
@@ -1,7 +1,7 @@
 #!/usr/bin
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
-        "facility": "sv4",
+        "facility": "sg4",
         "plan": "c3.medium.x86",
         "hostname": "rj-da-windows-test",
         "description": "RJ Windows Test Deploy",

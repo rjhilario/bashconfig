ade99f25d648094578b666f8fd0f20feb8e47122
RJ Hilario <rodjonh@gmail.com>
2023-06-30 10:22:19 +0800

    Changed facility and server plan

--- bash/deploy-ubuntu20_04.sh
+++ bash/deploy-ubuntu20_04.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
-        "facility": "sv15",
-        "plan": "c3.medium.x86",
+        "facility": "da11",
+        "plan": "m3.small.x86",
         "hostname": "rj-da-ubuntu-test",
         "description": "RJ Kubernetes test node",
         "operating_system": "ubuntu_20_04",

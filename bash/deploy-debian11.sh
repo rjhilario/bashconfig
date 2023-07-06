5e8923c91c2582c3507c8d0e35a7677b26af99e9
RJ Hilario <rodjonh@gmail.com>
2023-07-06 10:36:34 +0800

    Changed facility

--- bash/deploy-debian11.sh
+++ bash/deploy-debian11.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
-        "facility": "da6",
+        "facility": "sg4",
         "plan": "c3.small.x86",
         "hostname": "rj-da-debian11-test",
         "description": "RJ Dallas deploy Debian 11",

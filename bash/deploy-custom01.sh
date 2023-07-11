d46cde7b45122779fcad61d53ea185251765786e
RJ Hilario <rodjonh@gmail.com>
2023-07-11 15:31:14 +0800

    Changed description

--- bash/deploy-custom01.sh
+++ bash/deploy-custom01.sh
@@ -4,7 +4,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "ht
         "facility": "da6",
         "plan": "c3.medium.x86",
         "hostname": "rj-custom-ipxe-test01",
-        "description": "RJ Custom Test",
+        "description": "RJ Custom Deployment",
         "operating_system": "custom_ipxe",
         "always_pxe": true,
         "ipxe_script_url": "https://boot.netboot.xyz/"

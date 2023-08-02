25a6e2cd30260628d133b0a502156ad03cddd168
RJ Hilario <rodjonh@gmail.com>
2023-08-02 10:02:53 +0800

    Changed server config

--- bash/deploy-win2016.sh
+++ bash/deploy-win2016.sh
@@ -2,7 +2,7 @@
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
         "facility": "sv15",
-        "plan": "c3.large.x86",
+        "plan": "m3.large.x86",
         "hostname": "rj-da-windows-test",
         "description": "RJ Windows Test Deploy",
         "operating_system": "windows_2016",

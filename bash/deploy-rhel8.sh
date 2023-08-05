e49dfd478870553f37c348690051b79674adf389
RJ Hilario <rodjonh@gmail.com>
2023-08-05 23:03:37 +0800

    Changed server config

--- bash/deploy-rhel8.sh
+++ bash/deploy-rhel8.sh
@@ -2,7 +2,7 @@
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
         "facility": "sv15",
-        "plan": "c3.small.x86",
+        "plan": "m3.small.x86",
         "hostname": "rj-da-rheltest",
         "description": "RJ Test Deploy RHEL8",
         "operating_system": "rhel_8",

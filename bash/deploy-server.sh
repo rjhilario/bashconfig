414526e20c61711c0840a88646718245b903532d
RJ Hilario <rodjonh@gmail.com>
2023-08-23 19:19:19 +0800

    Edited config

--- bash/deploy-server.sh
+++ bash/deploy-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" 'https://api.equinix.com/metal/v1/projects/${project_id}/devices' -d \
     '{
-        "facility": "sv15",
+        "facility": "da6",
         "plan": "c3.small.x86",
         "hostname": "rj-da-n3.xlarge.test",
         "description": "RJ Test Deploy m3.small.x86",

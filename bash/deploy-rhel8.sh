3baf94d5a8d8b8e738ac452df009a59be14a522b
RJ Hilario <rodjonh@gmail.com>
2023-09-01 21:05:21 +0800

    Changed facility

--- bash/deploy-rhel8.sh
+++ bash/deploy-rhel8.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
-        "facility": "da6",
+        "facility": "sv15",
         "plan": "c3.small.x86",
         "hostname": "rj-da-rheltest",
         "description": "RJ Test Deploy RHEL8",

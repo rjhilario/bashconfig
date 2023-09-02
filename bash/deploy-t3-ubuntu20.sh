7bc5ea070dd363438e709ec05c797380c126d864
RJ Hilario <rodjonh@gmail.com>
2023-09-02 21:05:21 +0800

    Edited server config

--- bash/deploy-t3-ubuntu20.sh
+++ bash/deploy-t3-ubuntu20.sh
@@ -2,7 +2,7 @@
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" 'https://api.equinix.com/metal/v1/projects/${project_id}/devices' -d \
     '{
         "facility": "da6",
-        "plan": "t3.small.x86",
+        "plan": "c3.small.x86",
         "hostname": "rj-da-t3.small.x86-test",
         "description": "RJ Dallas Deployment Ubuntu 20.04 t3.small.x86",
         "operating_system": "ubuntu_20_04",

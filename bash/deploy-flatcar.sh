c65e47b2bdc5abc31ac54be1f3e4a49b03c72649
RJ Hilario <rodjonh@gmail.com>
2023-09-02 20:27:04 +0800

    Changed facility

--- bash/deploy-flatcar.sh
+++ bash/deploy-flatcar.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
-        "facility": "sv15",
+        "facility": "da6",
         "plan": "g2.large.x86",
         "hostname": "rj-da-flatcar-test",
         "description": "RJ Dallas deploy Flatcar Linux",

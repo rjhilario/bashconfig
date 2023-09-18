c7104c9009361a6995c9503c080504d16418e495
RJ Hilario <rodjonh@gmail.com>
2023-09-18 16:26:30 +0800

    Changed facility

--- bash/deploy-t3_small.sh
+++ bash/deploy-t3_small.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" 'https://api.equinix.com/metal/v1/projects/${project_id}/devices' -d \
     '{
-        "facility": "da6",
+        "facility": "sv15",
         "plan": "t3.small.x86",
         "hostname": "rj-da-t3.small.x86-test",
         "description": "RJ t3.small.x86 test deploy",

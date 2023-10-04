9a79f83a9ffeba4b729813ea5b121eb876fe0ca6
RJ Hilario <rodjonh@gmail.com>
2023-10-04 11:54:51 +0800

    Added entry for Rocky Linux deployment

---
+++ bash/deploy-rocky9.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
+    '{
+        "facility": "sg4",
+        "plan": "c3.small.x86",
+        "hostname": "rj-da-rheltest",
+        "description": "RJ Test Deploy RHEL8",
+        "operating_system": "rhel_8",
+        "tags": ["RHEL8 Test"]
+        }'

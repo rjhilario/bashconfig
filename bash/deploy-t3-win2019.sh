7c57c86166f8d3319950906a892dd9de584ae4e0
RJ Hilario <rodjonh@gmail.com>
2023-10-04 11:26:32 +0800

    Edited hostname

--- bash/deploy-t3-win2019.sh
+++ bash/deploy-t3-win2019.sh
@@ -3,7 +3,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" 'ht
     '{
         "facility": "da6",
         "plan": "t3.small.x86",
-        "hostname": "rj-da-windows-test",
+        "hostname": "rj-da-windows-t3",
         "description": "RJ Domain Controller",
         "operating_system": "windows_2019",
         "tags": ["shell script deploy"]

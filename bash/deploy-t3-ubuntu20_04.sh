298972380f7f8b33f06dfc66d76763a558feb0ce
RJ Hilario <rodjonh@gmail.com>
2023-10-05 13:55:01 +0800

    Edited facility

--- bash/deploy-t3-ubuntu20_04.sh
+++ bash/deploy-t3-ubuntu20_04.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
-        "facility": "da6",
+        "facility": "am6",
         "plan": "t3.small.x86",
         "hostname": "rj-da-t3-ubuntu",
         "description": "RJ-t3-ubuntu",

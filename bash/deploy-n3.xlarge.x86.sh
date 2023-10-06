80a7bf99cd4215c303f7628e0680fb410687da4c
RJ Hilario <rodjonh@gmail.com>
2023-10-06 10:09:56 +0800

    Changed to correct n3.xlarge.x86 server config

--- bash/deploy-n3.xlarge.x86.sh
+++ bash/deploy-n3.xlarge.x86.sh
@@ -2,8 +2,8 @@
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
         "facility": "am6",
-        "plan": "c3.small.x86",
-        "hostname": "rj-da-ubuntu-test",
+        "plan": "n3.xlarge.x86",
+        "hostname": "rj-am-n3.xlarge.x86",
         "description": "RJ Kubernetes test node",
         "operating_system": "ubuntu_20_04",
         "tags": ["shell script deploy"]

9fe78829807bc996d8497d0d8a4f399e87469167
RJ Hilario <rodjonh@gmail.com>
2023-10-06 23:34:44 +0800

    Changed to n3.xlarge.x86

--- bash/deploy-rocky9.sh
+++ bash/deploy-rocky9.sh
@@ -2,7 +2,7 @@
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
         "facility": "sg4",
-        "plan": "c3.small.x86",
+        "plan": "n3.xlarge.x86",
         "hostname": "rj-sg-rocky9",
         "description": "RJ Rocky 9 Deployment",
         "operating_system": "rocky_9",

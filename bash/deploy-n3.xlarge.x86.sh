640a91b47da4d23b222f0b7ac21c1cca9b8110f7
RJ Hilario <rodjonh@gmail.com>
2023-10-06 10:09:03 +0800

    Added new deployment for n3.xlarge.x86

---
+++ bash/deploy-n3.xlarge.x86.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
+    '{
+        "facility": "am6",
+        "plan": "c3.small.x86",
+        "hostname": "rj-da-ubuntu-test",
+        "description": "RJ Kubernetes test node",
+        "operating_system": "ubuntu_20_04",
+        "tags": ["shell script deploy"]
+        }'

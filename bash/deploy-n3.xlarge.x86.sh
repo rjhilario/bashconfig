890cda77bac2169456784ed32ff7355e2c929805
RJ Hilario <rodjonh@gmail.com>
2023-10-06 23:33:58 +0800

    Edited OS to Ubuntu 22.04

--- bash/deploy-n3.xlarge.x86.sh
+++ bash/deploy-n3.xlarge.x86.sh
@@ -5,6 +5,6 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "ht
         "plan": "n3.xlarge.x86",
         "hostname": "rj-am-n3.xlarge.x86",
         "description": "RJ n3.xlarge.x86 deployment",
-        "operating_system": "ubuntu_20_04",
+        "operating_system": "ubuntu_22_04",
         "tags": ["shell script deploy"]
         }'

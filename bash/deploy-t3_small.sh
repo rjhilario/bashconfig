b6def9ad08bb698064c8921b2f9871b826d07bce
RJ Hilario <rodjonh@gmail.com>
2023-10-13 21:02:44 +0800

    Edited hostname

--- bash/deploy-t3_small.sh
+++ bash/deploy-t3_small.sh
@@ -3,7 +3,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" 'ht
     '{
         "facility": "da6",
         "plan": "t3.small.x86",
-        "hostname": "rj-da-t3.small.x86-test",
+        "hostname": "rj-da-t3.small-test",
         "description": "RJ t3.small.x86 test deploy",
         "operating_system": "ubuntu_22_04",
         "tags": ["shell script deploy"],

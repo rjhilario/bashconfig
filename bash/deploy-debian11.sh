f413006dc8c314ea565947c1f1c7952c9a14c093
RJ Hilario <rodjonh@gmail.com>
2023-10-14 13:28:55 +0800

    Changed status

--- bash/deploy-debian11.sh
+++ bash/deploy-debian11.sh
@@ -2,7 +2,7 @@
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
         "facility": "am6",
-        "plan": "c3.small.x86",
+        "plan": "m3.large.x86",
         "hostname": "rj-da-debian11-test",
         "description": "RJ Dallas deploy Debian 11",
         "operating_system": "debian_11",

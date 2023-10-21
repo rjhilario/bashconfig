38f19b9ff97192e24af954e13433348bf709e40b
RJ Hilario <rodjonh@gmail.com>
2023-10-21 20:46:37 +0800

    Corrected facility for t3.small.x86, only in Dallas

--- bash/deploy-t3-ubuntu20.sh
+++ bash/deploy-t3-ubuntu20.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" 'https://api.equinix.com/metal/v1/projects/${project_id}/devices' -d \
     '{
-        "facility": "am6",
+        "facility": "da6",
         "plan": "c3.small.x86",
         "hostname": "rj-da-t3test",
         "description": "RJ Dallas Deployment Ubuntu 20.04 t3.small.x86",

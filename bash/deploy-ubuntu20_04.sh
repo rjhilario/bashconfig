42242f15da1b2346682cffb56cdc2f095cfeaf17
RJ Hilario <rodjonh@gmail.com>
2023-10-28 18:42:51 +0800

    Edited server config

--- bash/deploy-ubuntu20_04.sh
+++ bash/deploy-ubuntu20_04.sh
@@ -2,7 +2,7 @@
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
         "facility": "fr2",
-        "plan": "c3.small.x86",
+        "plan": "n3.xlarge.x86",
         "hostname": "rj-da-ubuntu-test",
         "description": "RJ Kubernetes test node",
         "operating_system": "ubuntu_20_04",

e88337018a103064a339c70c5847145dbf59ad12
RJ Hilario <rodjonh@gmail.com>
2023-11-02 23:45:18 +0800

    Edited description

--- bash/deploy-t3_small.sh
+++ bash/deploy-t3_small.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" 'https://api.equinix.com/metal/v1/projects/${project_id}/devices' -d \
     '{
-        "facility": "da6",
+        "facility": "da62",
         "plan": "t3.small.x86",
         "hostname": "rj-da-t3.small-test",
-        "description": "RJ t3.small.x86 test deploy",
+        "description": "Test for t3.small.x86",
         "operating_system": "ubuntu_22_04",
         "tags": ["shell script deploy"],
         }'

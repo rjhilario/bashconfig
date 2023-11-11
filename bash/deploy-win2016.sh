45a7487b2d1cdf82a20d59b2b465922880a01400
RJ Hilario <rodjonh@gmail.com>
2023-11-11 15:50:58 +0800

    Edited configuration

--- bash/deploy-win2016.sh
+++ bash/deploy-win2016.sh
@@ -1,7 +1,7 @@
 #!/usr/bin
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
-        "facility": "sg4",
+        "facility": "am6",
         "plan": "c3.medium.x86",
         "hostname": "rj-da-windows2016-test",
         "description": "RJ Windows Test Deploy",

db70863318f3e1e0410e47cd94feebe4539b8778
RJ Hilario <rodjonh@gmail.com>
2023-11-11 15:50:12 +0800

    Edited configuration

--- bash/deploy-rocky9.sh
+++ bash/deploy-rocky9.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
-        "facility": "am6",
+        "facility": "ty4",
         "plan": "c3.small.x86",
         "hostname": "rj-sg-rocky9",
         "description": "RJ Rocky 9 Deployment",

6cb06dc84c082b43cd153db0daa85af8a232c371
RJ Hilario <rodjonh@gmail.com>
2023-12-08 10:34:51 +0800

    Edited configuration

--- bash/deploy-win2019.sh
+++ bash/deploy-win2019.sh
@@ -2,7 +2,7 @@
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
         "facility": "am6",
-        "plan": "n3.xlarge.86",
+        "plan": "c3.medium.86",
         "hostname": "rj-da-win2019",
         "description": "RJ Windows 2019 Test",
         "operating_system": "windows_2019",

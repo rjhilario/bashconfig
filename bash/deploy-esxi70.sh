8c124da48851baa55bd965e530a0bb488cd829ce
RJ Hilario <rodjonh@gmail.com>
2023-12-09 19:12:40 +0800

    Edited config

--- bash/deploy-esxi70.sh
+++ bash/deploy-esxi70.sh
@@ -2,7 +2,7 @@
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" 'https://api.equinix.com/metal/v1/projects/${project_id}/devices' -d \
     '{
         "facility": "fr2",
-        "plan": "c3.medium.x86",
+        "plan": "n3.xlarge.x86",
         "hostname": "rj-esxi70-test",
         "description": "RJ ESXi 7.0 Test Deploy",
         "operating_system": "vmware_esxi_7_0",

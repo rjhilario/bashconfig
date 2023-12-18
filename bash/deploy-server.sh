820a575f5a7438337c4386bc51b7ce4977234c5f
RJ Hilario <rodjonh@gmail.com>
2023-12-18 12:42:51 +0800

    Edited facility

--- bash/deploy-server.sh
+++ bash/deploy-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" 'https://api.equinix.com/metal/v1/projects/${project_id}/devices' -d \
     '{
-        "facility": "ty4",
+        "facility": "am6",
         "plan": "c3.small.x86",
         "hostname": "rj-da-c3.small",
         "description": "RJ Test c3.small.x86",

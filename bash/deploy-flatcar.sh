533f2a221d7609fa9e95d64fe6e53a454c3ce87a
RJ Hilario <rodjonh@gmail.com>
2023-12-22 22:52:20 +0800

    Edited configuration

--- bash/deploy-flatcar.sh
+++ bash/deploy-flatcar.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
-        "facility": "am6",
+        "facility": "ty4",
         "plan": "c3.small.x86",
         "hostname": "rj-da-flatcar-test",
         "description": "RJ Dallas deploy Flatcar Linux",

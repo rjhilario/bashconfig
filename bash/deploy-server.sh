17d49413331c10136c9f776e7dbd520adcc523df
RJ Hilario <rodjonh@gmail.com>
2023-12-27 13:09:21 +0800

    Changed server configuration

--- bash/deploy-server.sh
+++ bash/deploy-server.sh
@@ -2,7 +2,7 @@
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" 'https://api.equinix.com/metal/v1/projects/${project_id}/devices' -d \
     '{
         "facility": "sv4",
-        "plan": "c3.small.x86",
+        "plan": "c3.medium.x86",
         "hostname": "rj-da-c3.small",
         "description": "RJ Test c3.small.x86",
         "operating_system": "ubuntu_20_04",

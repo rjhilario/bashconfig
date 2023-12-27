0662f97299ab11a8dc348ae45d21a76b2edd7a34
RJ Hilario <rodjonh@gmail.com>
2023-12-27 13:11:21 +0800

    Changed facility

--- bash/deploy-esxi70.sh
+++ bash/deploy-esxi70.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" 'https://api.equinix.com/metal/v1/projects/${project_id}/devices' -d \
     '{
-        "facility": "fr2",
+        "facility": "am6",
         "plan": "g2.xlarge.x86",
         "hostname": "rj-esxi70-test",
         "description": "RJ ESXi 7.0 Test Deploy",

97ac8ff2fdbe65ed334ae41215b6a6d1fbf90477
RJ Hilario <rodjonh@gmail.com>
2023-12-28 10:39:07 +0800

    Edited tags

--- bash/deploy-ubuntu22_04.sh
+++ bash/deploy-ubuntu22_04.sh
@@ -6,5 +6,5 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "ht
         "hostname": "rj-fr-ubuntu22",
         "description": "RJ Ubuntu 22.04 Deployment",
         "operating_system": "ubuntu_22_04",
-        "tags": ["shell script deploy"]
+        "tags": ["shell script deployment"]
         }'

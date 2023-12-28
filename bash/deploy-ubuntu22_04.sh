39a33fab1c33f26b0d19d5497985f7cc365f5858
RJ Hilario <rodjonh@gmail.com>
2023-12-28 10:38:39 +0800

    Edited desecription

--- bash/deploy-ubuntu22_04.sh
+++ bash/deploy-ubuntu22_04.sh
@@ -4,7 +4,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "ht
         "facility": "fr2",
         "plan": "c3.small.x86",
         "hostname": "rj-fr-ubuntu22",
-        "description": "RJ Ubuntu 22 Test",
+        "description": "RJ Ubuntu 22.04 Deployment",
         "operating_system": "ubuntu_22_04",
         "tags": ["shell script deploy"]
         }'

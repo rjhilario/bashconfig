bb54bc86bd89112d423d57c88457c759fa48cf3c
RJ Hilario <rodjonh@gmail.com>
2023-12-28 10:37:37 +0800

    Changed server configuration

--- bash/deploy-ubuntu22_04.sh
+++ bash/deploy-ubuntu22_04.sh
@@ -2,7 +2,7 @@
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
         "facility": "fr2",
-        "plan": "n3.xlarge.x86",
+        "plan": "c3.small.x86",
         "hostname": "rj-da-ubuntu22-test02",
         "description": "RJ Ubuntu 22 Test",
         "operating_system": "ubuntu_22_04",

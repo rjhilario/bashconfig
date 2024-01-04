1ef0fe876f4e81090171b3dafae6e5ba5cf9f489
RJ Hilario <rodjonh@gmail.com>
2024-01-04 10:09:46 +0800

    Edited hostname

--- bash/deploy-rhel8.sh
+++ bash/deploy-rhel8.sh
@@ -3,7 +3,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "ht
     '{
         "facility": "am6",
         "plan": "c3.medium.x86",
-        "hostname": "rj-da-rheltest",
+        "hostname": "rj-am-rheltest",
         "description": "RJ Test Deploy RHEL8",
         "operating_system": "rhel_8",
         "tags": ["RHEL8 Test"]

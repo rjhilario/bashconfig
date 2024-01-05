64fd8ecba512a9bf42aae9972f7f5f4aa5c9f1e9
RJ Hilario <rodjonh@gmail.com>
2024-01-05 08:21:25 +0800

    Changed server plan

--- bash/deploy-server.sh
+++ bash/deploy-server.sh
@@ -2,7 +2,7 @@
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" 'https://api.equinix.com/metal/v1/projects/${project_id}/devices' -d \
     '{
         "facility": "ty4",
-        "plan": "c3.medium.x86",
+        "plan": "c3.small.x86",
         "hostname": "rj-ty-server-test",
         "description": "RJ Test Deploy",
         "operating_system": "ubuntu_20_04",

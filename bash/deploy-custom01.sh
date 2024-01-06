17096dd1556359ef4d2bbf8c94be0c5b1005711d
RJ Hilario <rodjonh@gmail.com>
2024-01-06 11:29:51 +0800

    Changed facility

--- bash/deploy-custom01.sh
+++ bash/deploy-custom01.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
-        "facility": "fr2",
+        "facility": "ty4",
         "plan": "c3.medium.x86",
         "hostname": "rj-custom-test",
         "description": "RJ Custom Deployment",

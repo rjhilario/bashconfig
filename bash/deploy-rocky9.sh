8765cb812c59a75a3d444bd997ff753a8b416735
RJ Hilario <rodjonh@gmail.com>
2024-01-20 09:12:01 +0800

    Edited hostname

--- bash/deploy-rocky9.sh
+++ bash/deploy-rocky9.sh
@@ -3,7 +3,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "ht
     '{
         "facility": "fr2",
         "plan": "c3.medium.x86",
-        "hostname": "rj-am-rocky9",
+        "hostname": "rj-fr-rocky9",
         "description": "RJ Rocky 9 Test",
         "operating_system": "rocky_9",
         "tags": ["Rocky 9 Test"]

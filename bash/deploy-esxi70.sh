3b895465eb71faae03e8f184e6c4a08287f780ea
RJ Hilario <rodjonh@gmail.com>
2024-01-24 14:20:54 +0800

    Edited hostname

--- bash/deploy-esxi70.sh
+++ bash/deploy-esxi70.sh
@@ -3,7 +3,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" 'ht
     '{
         "facility": "am6",
         "plan": "c3.large.x86",
-        "hostname": "rj-ty-esxi70",
+        "hostname": "rj-am-esxi70",
         "description": "RJ ESXi 7.0 Test Deploy",
         "operating_system": "vmware_esxi_7_0",
         "tags": ["shell script deploy"]

eed40d1784c8ff96881bcd009a8ca5d91ad16125
RJ Hilario <rodjonh@gmail.com>
2024-01-24 14:21:23 +0800

    Changed description

--- bash/deploy-esxi70.sh
+++ bash/deploy-esxi70.sh
@@ -4,7 +4,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" 'ht
         "facility": "am6",
         "plan": "c3.large.x86",
         "hostname": "rj-am-esxi70",
-        "description": "RJ ESXi 7.0 Test Deploy",
+        "description": "RJ ESXi 7.0 Deploy in AM",
         "operating_system": "vmware_esxi_7_0",
         "tags": ["shell script deploy"]
         }'

3cfe541b1396ad56fe000b43306eb36d09594386
RJ Hilario <rodjonh@gmail.com>
2024-01-24 14:19:28 +0800

    Changed description

--- bash/deploy-n3.xlarge.x86.sh
+++ bash/deploy-n3.xlarge.x86.sh
@@ -4,7 +4,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "ht
         "facility": "ty4",
         "plan": "n3.xlarge.x86",
         "hostname": "rj-ty-n3.xlarge.x86",
-        "description": "RJ n3.xlarge.x86 deployment",
+        "description": "RJ n3.xlarge.x86 deployment in TY",
         "operating_system": "ubuntu_22_04",
         "tags": ["shell script deploy"]
         }'

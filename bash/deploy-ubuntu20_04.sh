7bb5a1ed54a802a5b81919f0ab5c5a78cc7f2a92
RJ Hilario <rodjonh@gmail.com>
2024-01-25 11:12:58 +0800

    Edited hostname

--- bash/deploy-ubuntu20_04.sh
+++ bash/deploy-ubuntu20_04.sh
@@ -3,7 +3,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "ht
     '{
         "facility": "fr2",
         "plan": "c3.medium.x86",
-        "hostname": "rj-da-ubuntu-test",
+        "hostname": "rj-fr-ubuntu20.04",
         "description": "RJ Kubernetes test node",
         "operating_system": "ubuntu_20_04",
         "tags": ["shell script deploy"]

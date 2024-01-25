da32b871f0fbbe2aa0618994a900d3aa3f38d0dc
RJ Hilario <rodjonh@gmail.com>
2024-01-25 11:16:55 +0800

    Changed description

--- bash/deploy-rhel8.sh
+++ bash/deploy-rhel8.sh
@@ -4,7 +4,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "ht
         "facility": "fr2",
         "plan": "c3.small.x86",
         "hostname": "rj-am-rhel8",
-        "description": "RJ Deploy RHEL8 in AM",
+        "description": "RJ Deploy RHEL8 in FR",
         "operating_system": "rhel_8",
         "tags": ["RHEL8 Test"]
         }'

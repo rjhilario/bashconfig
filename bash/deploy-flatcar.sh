d850a83f0e7bea0afa222f5fb0711f6f5eb85424
RJ Hilario <rodjonh@gmail.com>
2024-01-30 19:31:45 +0800

    Changed hostname

--- bash/deploy-flatcar.sh
+++ bash/deploy-flatcar.sh
@@ -3,7 +3,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "ht
     '{
         "facility": "fr2",
         "plan": "c3.medium.x86",
-        "hostname": "rj-fr-flatcar-test",
+        "hostname": "rj-fr-flatcar-linux",
         "description": "RJ Dallas deploy Flatcar Linux",
         "operating_system": "flatcar_linux",
         "tags": ["shell script deploy"]

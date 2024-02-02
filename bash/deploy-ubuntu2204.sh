d6364c56e379ced7f47d7c2f2d63726c860e3f8c
RJ Hilario <rodjonh@gmail.com>
2024-02-02 22:32:56 +0800

    Changed server plan to c3.medium.x86

--- bash/deploy-ubuntu2204.sh
+++ bash/deploy-ubuntu2204.sh
@@ -2,7 +2,7 @@
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
         "facility": "fr2",
-        "plan": "c3.small.x86",
+        "plan": "c3.medium.x86",
         "hostname": "rj-da-ubuntu22.04",
         "description": "RJ Ubuntu 220.04 Deployment",
         "operating_system": "ubuntu_22_04",

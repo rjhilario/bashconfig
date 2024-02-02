c8134290f46045f7b17a5d82559d862d3fb9c92a
RJ Hilario <rodjonh@gmail.com>
2024-02-02 22:33:18 +0800

    Changed hostname

--- bash/deploy-ubuntu2204.sh
+++ bash/deploy-ubuntu2204.sh
@@ -3,7 +3,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "ht
     '{
         "facility": "fr2",
         "plan": "c3.medium.x86",
-        "hostname": "rj-da-ubuntu22.04",
+        "hostname": "rj-fr-ubuntu22.04",
         "description": "RJ Ubuntu 220.04 Deployment",
         "operating_system": "ubuntu_22_04",
         "tags": ["shell script deploy"]

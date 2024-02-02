ccc8556d21d91ef35af7bbf5d5d3c148c5f6f173
RJ Hilario <rodjonh@gmail.com>
2024-02-02 22:37:52 +0800

    Changed facility to SG4

--- bash/deploy-win2016.sh
+++ bash/deploy-win2016.sh
@@ -1,7 +1,7 @@
 #!/usr/bin
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
-        "facility": "am6",
+        "facility": "sg4",
         "plan": "c3.medium.x86",
         "hostname": "rj-fr-win2k16",
         "description": "RJ Windows Server 2016 Deploy in FR",

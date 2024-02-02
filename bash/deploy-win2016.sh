b9729ff0c545b1c82758a8d52953f8624f043033
RJ Hilario <rodjonh@gmail.com>
2024-02-02 22:38:25 +0800

    Changed server plan to m3.large.x86

--- bash/deploy-win2016.sh
+++ bash/deploy-win2016.sh
@@ -2,7 +2,7 @@
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
         "facility": "sg4",
-        "plan": "c3.medium.x86",
+        "plan": "m3.large.x86",
         "hostname": "rj-fr-win2k16",
         "description": "RJ Windows Server 2016 Deploy in FR",
         "operating_system": "windows_2016",

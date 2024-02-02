34e813c4b1b8c4d5f132f71f8e5c371f31d433aa
RJ Hilario <rodjonh@gmail.com>
2024-02-02 22:39:32 +0800

    Edited description

--- bash/deploy-win2016.sh
+++ bash/deploy-win2016.sh
@@ -4,7 +4,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "ht
         "facility": "sg4",
         "plan": "m3.large.x86",
         "hostname": "rj-sg-win2016",
-        "description": "RJ Windows Server 2016 Deploy in FR",
+        "description": "RJ Windows Server 2016 Deployment in SG",
         "operating_system": "windows_2016",
         "tags": ["shell script deploy"]
         }'

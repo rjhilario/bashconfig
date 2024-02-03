226bc1f36ca542bccda567077be5ef25e8f03549
RJ Hilario <rodjonh@gmail.com>
2024-02-03 19:01:01 +0800

    Changed facility

--- bash/deploy-ubuntu22_04.sh
+++ bash/deploy-ubuntu22_04.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
-        "facility": "am6",
+        "facility": "sg4",
         "plan": "c3.small.x86",
         "hostname": "rj-fr-ubuntu22",
         "description": "RJ Ubuntu 22.04 Deployment",

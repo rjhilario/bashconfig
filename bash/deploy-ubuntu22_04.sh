8de039a5b555af7678448a7166164d7b2db6e241
RJ Hilario <rodjonh@gmail.com>
2024-02-03 19:02:24 +0800

    Edited hostname

--- bash/deploy-ubuntu22_04.sh
+++ bash/deploy-ubuntu22_04.sh
@@ -3,7 +3,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "ht
     '{
         "facility": "sg4",
         "plan": "m3.large.x86",
-        "hostname": "rj-fr-ubuntu22",
+        "hostname": "rj-sg-ubuntu22.04",
         "description": "RJ Ubuntu 22.04 Deployment",
         "operating_system": "ubuntu_22_04",
         "tags": ["shell script deployment"]

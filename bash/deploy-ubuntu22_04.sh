dd0789e20918ab7a4876c2962df136f1eced6c61
RJ Hilario <rodjonh@gmail.com>
2024-02-03 19:01:47 +0800

    Changed server config to m3.large.x86

--- bash/deploy-ubuntu22_04.sh
+++ bash/deploy-ubuntu22_04.sh
@@ -2,7 +2,7 @@
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
         "facility": "sg4",
-        "plan": "c3.small.x86",
+        "plan": "m3.large.x86",
         "hostname": "rj-fr-ubuntu22",
         "description": "RJ Ubuntu 22.04 Deployment",
         "operating_system": "ubuntu_22_04",

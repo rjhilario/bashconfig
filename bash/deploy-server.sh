5eaabe3f80448e359a8786a61d70904400318a8b
RJ Hilario <rodjonh@gmail.com>
2024-02-07 15:22:44 +0800

    Changed OS slug

--- bash/deploy-server.sh
+++ bash/deploy-server.sh
@@ -5,6 +5,6 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" 'ht
         "plan": "c3.small.x86",
         "hostname": "rj-sg-c3.small.x86",
         "description": "RJ Deploy Server Ubuntu 20.04 in SG4",
-        "operating_system": "ubuntu_20_04",
+        "operating_system": "ubuntu_22_04",
         "tags": ["Shell Script Deployment"],
         }'

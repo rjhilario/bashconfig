6c89354e7c59d7d9102ff1873619b6f1d017904e
RJ Hilario <rodjonh@gmail.com>
2024-02-07 15:21:43 +0800

    Changed hostname

--- bash/deploy-server.sh
+++ bash/deploy-server.sh
@@ -3,7 +3,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" 'ht
     '{
         "facility": "sg4",
         "plan": "c3.small.x86",
-        "hostname": "rj-am-c3.small.x86",
+        "hostname": "rj-sg-c3.small.x86",
         "description": "RJ Deploy Server Ubuntu 20.04 in AM6",
         "operating_system": "ubuntu_20_04",
         "tags": ["Shell Script Deployment"],

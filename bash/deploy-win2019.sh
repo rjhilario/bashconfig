c897be53b08bf9067af1b94cf9d325410a100327
RJ Hilario <rodjonh@gmail.com>
2024-02-08 15:31:36 +0800

    Changed facility to SG4

--- bash/deploy-win2019.sh
+++ bash/deploy-win2019.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
-        "facility": "am6",
+        "facility": "sg4",
         "plan": "c3.small.86",
         "hostname": "rj-am-win2019",
         "description": "RJ Windows Server 2019 Deployment in AM",

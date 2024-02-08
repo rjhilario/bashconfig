1913de0dc516b9cd902898dda6f2e7ca6e8d3631
RJ Hilario <rodjonh@gmail.com>
2024-02-08 15:35:25 +0800

    Edited description

--- bash/deploy-rocky9.sh
+++ bash/deploy-rocky9.sh
@@ -4,7 +4,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "ht
         "facility": "sg4",
         "plan": "m3.large.x86",
         "hostname": "rj-sg-rocky9",
-        "description": "RJ Rocky 9 Test",
+        "description": "RJ Rocky 9 Linux Deployment in SG",
         "operating_system": "rocky_9",
         "tags": ["Rocky 9 Test"]
         }'

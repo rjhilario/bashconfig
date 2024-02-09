4c91e5fd7a6a8fa582d78433d35c75cd13283014
RJ Hilario <rodjonh@gmail.com>
2024-02-10 06:31:29 +0800

    Edited hostname

--- bash/deploy-custom01.sh
+++ bash/deploy-custom01.sh
@@ -3,7 +3,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "ht
     '{
         "facility": "sg4",
         "plan": "m3.large.x86",
-        "hostname": "rj-custom-deploy",
+        "hostname": "rj-sg-custom-deploy",
         "description": "RJ Custom in SV",
         "operating_system": "custom_ipxe",
         "always_pxe": true,

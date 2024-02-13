d4655f4328c751b88475391fe5f4a948f6307060
RJ Hilario <rodjonh@gmail.com>
2024-02-13 18:43:05 +0800

    Changed hostname

--- bash/deploy-ubuntu20_04.sh
+++ bash/deploy-ubuntu20_04.sh
@@ -3,7 +3,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "ht
     '{
         "facility": "sg4",
         "plan": "c3.small.x86",
-        "hostname": "rj-fr-ubuntu20.04",
+        "hostname": "rj-sg-ubuntu20.04",
         "description": "RJ Ubuntu 20.04 deployment in FR",
         "operating_system": "ubuntu_20_04",
         "tags": ["shell script deploy"]

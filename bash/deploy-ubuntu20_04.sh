37e0816fdf2b1a3e226bf627ca16f47cda09798c
RJ Hilario <rodjonh@gmail.com>
2024-02-13 18:42:36 +0800

    Changed server plan

--- bash/deploy-ubuntu20_04.sh
+++ bash/deploy-ubuntu20_04.sh
@@ -2,7 +2,7 @@
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
         "facility": "sg4",
-        "plan": "c3.medium.x86",
+        "plan": "c3.small.x86",
         "hostname": "rj-fr-ubuntu20.04",
         "description": "RJ Ubuntu 20.04 deployment in FR",
         "operating_system": "ubuntu_20_04",

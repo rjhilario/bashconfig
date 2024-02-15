88426a09712459d482916682d5d3c29067eb7932
RJ Hilario <rodjonh@gmail.com>
2024-02-15 09:36:07 +0800

    Changed the facility back to DA6

--- bash/deploy-t3-windows2019.sh
+++ bash/deploy-t3-windows2019.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
-        "facility": "sv15",
+        "facility": "da6",
         "plan": "t3.small.x86",
         "hostname": "rj-da-windows-test",
         "description": "RJ Test Deploy Windows 2019 Test",

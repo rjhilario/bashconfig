b3197ee570d1c853fc4dad07ea45f1c43ad646b6
RJ Hilario <rodjonh@gmail.com>
2024-02-17 21:55:23 +0800

    Changed server config to m3.large.x86

--- bash/deploy-server.sh
+++ bash/deploy-server.sh
@@ -2,7 +2,7 @@
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" 'https://api.equinix.com/metal/v1/projects/${project_id}/devices' -d \
     '{
         "facility": "ty11",
-        "plan": "c3.small.x86",
+        "plan": "m3.large.x86",
         "hostname": "rj-sg-c3.small.x86",
         "description": "RJ Deploy Server Ubuntu 20.04 in SG4",
         "operating_system": "ubuntu_22_04",

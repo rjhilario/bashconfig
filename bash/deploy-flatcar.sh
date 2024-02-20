708a7678176698f948bc899dda6f9aa126d42425
RJ Hilario <rodjonh@gmail.com>
2024-02-21 07:56:01 +0800

    Edited hostname

--- bash/deploy-flatcar.sh
+++ bash/deploy-flatcar.sh
@@ -3,7 +3,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "ht
     '{
         "facility": "ty11",
         "plan": "m3.large.x86",
-        "hostname": "rj-fr-flatcar-linux",
+        "hostname": "rj-ty-flatcar-linux",
         "description": "RJ Flatcar Linux Deployment in FR",
         "operating_system": "flatcar_linux",
         "tags": ["shell script deploy"]

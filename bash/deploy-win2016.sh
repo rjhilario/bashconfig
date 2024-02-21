45cd175cc2e9a57464c5b2aa48414cfd706f3e71
RJ Hilario <rodjonh@gmail.com>
2024-02-22 07:10:49 +0800

    Changed facility to TY11

--- bash/deploy-win2016.sh
+++ bash/deploy-win2016.sh
@@ -1,7 +1,7 @@
 #!/usr/bin
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
-        "facility": "sg4",
+        "facility": "ty11",
         "plan": "n3.xlarge.x86",
         "hostname": "rj-sg-win2016",
         "description": "RJ Windows Server 2016 Deployment in SG",

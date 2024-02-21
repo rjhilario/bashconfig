ffa6016d10a90ed6f263bab47e6b0438c576ed7a
RJ Hilario <rodjonh@gmail.com>
2024-02-22 07:09:31 +0800

    Changed server configuration to n3.xlarge.x86

--- bash/deploy-win2016.sh
+++ bash/deploy-win2016.sh
@@ -2,7 +2,7 @@
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
         "facility": "sg4",
-        "plan": "m3.large.x86",
+        "plan": "n3.xlarge.x86",
         "hostname": "rj-sg-win2016",
         "description": "RJ Windows Server 2016 Deployment in SG",
         "operating_system": "windows_2016",

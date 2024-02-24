232020d571b31b6244c6762ff15953ccbb4d0f49
RJ Hilario <rodjonh@gmail.com>
2024-02-24 10:32:29 +0800

    Edited server plan

--- bash/deploy-ubuntu20_04.sh
+++ bash/deploy-ubuntu20_04.sh
@@ -2,7 +2,7 @@
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
         "facility": "ty11",
-        "plan": "c3.small.x86",
+        "plan": "m3.large.x86",
         "hostname": "rj-sg-ubuntu20.04",
         "description": "RJ Ubuntu 20.04 deployment in SG",
         "operating_system": "ubuntu_20_04",

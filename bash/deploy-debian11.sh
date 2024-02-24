aa4b780ebcf3cf7730965bc13fb6ac41f7c83eb4
RJ Hilario <rodjonh@gmail.com>
2024-02-24 10:29:38 +0800

    Changed facility to TY11

--- bash/deploy-debian11.sh
+++ bash/deploy-debian11.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
-        "facility": "ty4",
+        "facility": "ty11",
         "plan": "c3.small.x86",
         "hostname": "rj-sg-debian11",
         "description": "RJ Debian 11 Deployment in SG",

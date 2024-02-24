994ec1df59054dd1d3ef17a3249784f020117aaa
RJ Hilario <rodjonh@gmail.com>
2024-02-24 10:32:01 +0800

    Changed facility to TY11

--- bash/deploy-ubuntu20_04.sh
+++ bash/deploy-ubuntu20_04.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
-        "facility": "sg4",
+        "facility": "ty11",
         "plan": "c3.small.x86",
         "hostname": "rj-sg-ubuntu20.04",
         "description": "RJ Ubuntu 20.04 deployment in SG",

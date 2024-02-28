f495bde7b0b3f0f26be673bbbc3654a9fd2bc801
RJ Hilario <rodjonh@gmail.com>
2024-02-28 11:40:25 +0800

    Edited description

--- bash/deploy-custom02.sh
+++ bash/deploy-custom02.sh
@@ -4,7 +4,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "ht
         "facility": "sg4",
         "plan": "c3.small.x86",
         "hostname": "rj-sg-custom-deploy",
-        "description": "RJ Custom in TY",
+        "description": "RJ Custom in SG",
         "operating_system": "custom_ipxe",
         "always_pxe": true,
         "ipxe_script_url": "https://boot.netboot.xyz/"

35fc52abf79b84f2a0449ea10537c7ad2a162f54
RJ Hilario <rodjonh@gmail.com>
2024-02-28 11:38:06 +0800

    Edited hostname

--- bash/deploy-custom01.sh
+++ bash/deploy-custom01.sh
@@ -3,7 +3,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "ht
     '{
         "facility": "ty11",
         "plan": "c3.small.x86",
-        "hostname": "rj-sg-custom-deploy",
+        "hostname": "rj-ty-custom-deploy",
         "description": "RJ Custom in SG",
         "operating_system": "custom_ipxe",
         "always_pxe": true,

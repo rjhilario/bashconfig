6c123bd10f83f4ed824523aa44080df998f5a9a9
RJ Hilario <rodjonh@gmail.com>
2024-03-01 07:59:56 +0800

    Edited hostname

--- bash/deploy-ubuntu22_04.sh
+++ bash/deploy-ubuntu22_04.sh
@@ -3,7 +3,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "ht
     '{
         "facility": "hk2",
         "plan": "m3.small.x86",
-        "hostname": "rj-ty-ubuntu22.04",
+        "hostname": "rj-hk-ubuntu22.04",
         "description": "RJ Ubuntu 22.04 Deployment in TY",
         "operating_system": "ubuntu_22_04",
         "tags": ["shell script deployment"]

56f5555b4e9e33d841dfab05cb3597a16ab989d9
RJ Hilario <rodjonh@gmail.com>
2024-03-01 07:59:31 +0800

    Changed server plan to m3.small.x86

--- bash/deploy-ubuntu22_04.sh
+++ bash/deploy-ubuntu22_04.sh
@@ -2,7 +2,7 @@
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
         "facility": "hk2",
-        "plan": "m3.large.x86",
+        "plan": "m3.small.x86",
         "hostname": "rj-ty-ubuntu22.04",
         "description": "RJ Ubuntu 22.04 Deployment in TY",
         "operating_system": "ubuntu_22_04",

e7508b151b3e149fad2ae72dac5b42f7be9b8834
RJ Hilario <rodjonh@gmail.com>
2024-03-01 07:59:02 +0800

    Changed facility to HK2

--- bash/deploy-ubuntu22_04.sh
+++ bash/deploy-ubuntu22_04.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
-        "facility": "ty11",
+        "facility": "hk2",
         "plan": "m3.large.x86",
         "hostname": "rj-ty-ubuntu22.04",
         "description": "RJ Ubuntu 22.04 Deployment in TY",

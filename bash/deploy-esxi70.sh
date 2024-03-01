506c999223704171e1e9b1dd02f6922609240f73
RJ Hilario <rodjonh@gmail.com>
2024-03-01 08:03:06 +0800

    Edited description

--- bash/deploy-esxi70.sh
+++ bash/deploy-esxi70.sh
@@ -4,7 +4,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" 'ht
         "facility": "hk2",
         "plan": "c3.medium.x86",
         "hostname": "rj-hk-esxi70",
-        "description": "RJ ESXi 7.0 Deployment in TY",
+        "description": "RJ ESXi 7.0 Deployment in HK",
         "operating_system": "vmware_esxi_7_0",
         "tags": ["shell script deploy"]
         }'

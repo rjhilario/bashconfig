ae0865609c54248b8db4ee263087695ccbb0563c
RJ Hilario <rodjonh@gmail.com>
2024-03-01 08:02:11 +0800

    Changed server plan to c3.medium.x86

--- bash/deploy-esxi70.sh
+++ bash/deploy-esxi70.sh
@@ -2,7 +2,7 @@
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" 'https://api.equinix.com/metal/v1/projects/${project_id}/devices' -d \
     '{
         "facility": "hk2",
-        "plan": "n3.xlarge.x86",
+        "plan": "c3.medium.x86",
         "hostname": "rj-ty-esxi70",
         "description": "RJ ESXi 7.0 Deployment in TY",
         "operating_system": "vmware_esxi_7_0",

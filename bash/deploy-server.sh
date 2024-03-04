8e66a84f706d22e4a88f130fa0a335c42b9c5c1d
RJ Hilario <rodjonh@gmail.com>
2024-03-04 18:12:39 +0800

    Changed server config

--- bash/deploy-server.sh
+++ bash/deploy-server.sh
@@ -2,7 +2,7 @@
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" 'https://api.equinix.com/metal/v1/projects/${project_id}/devices' -d \
     '{
         "facility": "hk4",
-        "plan": "m3.large.x86",
+        "plan": "m3.small.x86",
         "hostname": "rj-ty-m3.large.x86",
         "description": "RJ Ubuntu 22.04 Deployment in TY",
         "operating_system": "ubuntu_22_04",

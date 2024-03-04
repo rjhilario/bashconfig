3906b508441b17a0c89e2b2b3ff74dfe4e468a5b
RJ Hilario <rodjonh@gmail.com>
2024-03-04 18:16:23 +0800

    Edited config

--- bash/deploy-rocky9.sh
+++ bash/deploy-rocky9.sh
@@ -4,7 +4,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "ht
         "facility": "hk4",
         "plan": "m3.small.x86",
         "hostname": "rj-hk-rocky9",
-        "description": "RJ Rocky 9 Linux Deployment in TY",
+        "description": "RJ Rocky 9 Linux Deployment in HK",
         "operating_system": "rocky_9",
         "tags": ["Rocky 9 Test"]
         }'

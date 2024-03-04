441f8e1479523b4e0c7600f734c07b7ae62db2a7
RJ Hilario <rodjonh@gmail.com>
2024-03-04 18:14:45 +0800

    Changed server config

--- bash/deploy-rocky9.sh
+++ bash/deploy-rocky9.sh
@@ -2,7 +2,7 @@
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
         "facility": "hk4",
-        "plan": "m3.large.x86",
+        "plan": "m3.small.x86",
         "hostname": "rj-ty-rocky9",
         "description": "RJ Rocky 9 Linux Deployment in TY",
         "operating_system": "rocky_9",

bf9a34013a0e764cd4ff9eedf8dc95a15cbd4feb
RJ Hilario <rodjonh@gmail.com>
2024-03-07 12:30:21 +0800

    Changed hostname

--- bash/deploy-win2019.sh
+++ bash/deploy-win2019.sh
@@ -3,7 +3,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "ht
     '{
         "facility": "hk4",
         "plan": "m3.small.x86",
-        "hostname": "rj-sg-win2019",
+        "hostname": "rj-hk-win2019",
         "description": "RJ Windows Server 2019 Deployment in SG",
         "operating_system": "windows_2019",
         "tags": ["shell script deploy"]

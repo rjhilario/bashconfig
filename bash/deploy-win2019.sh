3127ade236c9a596405dc72ee86cf6c3ae5e17ae
RJ Hilario <rodjonh@gmail.com>
2024-03-07 12:30:51 +0800

    Changed description

--- bash/deploy-win2019.sh
+++ bash/deploy-win2019.sh
@@ -4,7 +4,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "ht
         "facility": "hk4",
         "plan": "m3.small.x86",
         "hostname": "rj-hk-win2019",
-        "description": "RJ Windows Server 2019 Deployment in SG",
+        "description": "RJ Windows Server 2019 Deployment in HK",
         "operating_system": "windows_2019",
         "tags": ["shell script deploy"]
         }'

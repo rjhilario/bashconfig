2291c4ef293c0eb3b72e489276c229884f1ca6cf
RJ Hilario <rodjonh@gmail.com>
2024-03-07 12:29:56 +0800

    Changed server plan to m3.small.x86

--- bash/deploy-win2019.sh
+++ bash/deploy-win2019.sh
@@ -2,7 +2,7 @@
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
         "facility": "hk4",
-        "plan": "m3.large.86",
+        "plan": "m3.small.x86",
         "hostname": "rj-sg-win2019",
         "description": "RJ Windows Server 2019 Deployment in SG",
         "operating_system": "windows_2019",

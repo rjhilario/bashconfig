6f597728a9ecf0e5cefece153eef66bb14f9bb03
RJ Hilario <rodjonh@gmail.com>
2024-03-12 20:58:13 +0800

    Edted config

--- bash/deploy-ubuntu20_04.sh
+++ bash/deploy-ubuntu20_04.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
-        "facility": "ty11",
-        "plan": "m3.large.x86",
+        "facility": "hk4",
+        "plan": "m3.small.x86",
         "hostname": "rj-ty-ubuntu20.04",
         "description": "RJ Ubuntu 20.04 deployment in TY",
         "operating_system": "ubuntu_20_04",

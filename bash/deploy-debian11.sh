4b2fc3df63565216e2b209b6db8f5a7363e70dc1
RJ Hilario <rodjonh@gmail.com>
2024-03-12 20:55:35 +0800

    Changed server config

--- bash/deploy-debian11.sh
+++ bash/deploy-debian11.sh
@@ -2,7 +2,7 @@
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
         "facility": "hk4",
-        "plan": "m3.large.x86",
+        "plan": "m3.small.x86",
         "hostname": "rj-ty-debian11",
         "description": "RJ Debian 11 Deployment in TY",
         "operating_system": "debian_11",

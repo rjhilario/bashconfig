8acb9a3c1e69a4723e989988b0494bb414ae6b4a
RJ Hilario <rodjonh@gmail.com>
2024-03-12 20:56:52 +0800

    Changed hostname

--- bash/deploy-debian11.sh
+++ bash/deploy-debian11.sh
@@ -4,7 +4,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "ht
         "facility": "hk4",
         "plan": "m3.small.x86",
         "hostname": "rj-hk-debian11",
-        "description": "RJ Debian 11 Deployment in TY",
+        "description": "RJ Debian 11 Deployment in HK",
         "operating_system": "debian_11",
         "tags": ["shell script deploy"]
         }'

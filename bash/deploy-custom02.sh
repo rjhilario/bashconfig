62c75e105a5a6c39719f264b0d48133ca15125e0
RJ Hilario <rodjonh@gmail.com>
2024-03-14 18:02:33 +0800

    Changed facility to HK4

--- bash/deploy-custom02.sh
+++ bash/deploy-custom02.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
-        "facility": "sg4",
+        "facility": "hk4",
         "plan": "c3.small.x86",
         "hostname": "rj-sg-custom-deploy",
         "description": "RJ Custom in SG",

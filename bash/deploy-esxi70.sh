a092ae775a2fb0610af54af774471163c274722b
RJ Hilario <rodjonh@gmail.com>
2024-03-16 14:38:13 +0800

    Changed server config

--- bash/deploy-esxi70.sh
+++ bash/deploy-esxi70.sh
@@ -2,7 +2,7 @@
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" 'https://api.equinix.com/metal/v1/projects/${project_id}/devices' -d \
     '{
         "facility": "ny5",
-        "plan": "c3.medium.x86",
+        "plan": "c3.small.x86",
         "hostname": "rj-hk-esxi70",
         "description": "RJ ESXi 7.0 Deployment in HK",
         "operating_system": "vmware_esxi_7_0",

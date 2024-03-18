42e2ba23a5f0980d99f9ecba06826d199c88845c
RJ Hilario <rodjonh@gmail.com>
2024-03-18 17:38:59 +0800

    Changed server config to c3.small.x86

--- bash/deploy-rhel8.sh
+++ bash/deploy-rhel8.sh
@@ -2,7 +2,7 @@
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
         "facility": "ny5",
-        "plan": "m3.xlarge.x86",
+        "plan": "c3.small.x86",
         "hostname": "rj-ty-rhel8",
         "description": "RJ Deploy RHEL8 in TY",
         "operating_system": "rhel_8",

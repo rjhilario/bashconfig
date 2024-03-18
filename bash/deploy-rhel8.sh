858581f96abcf520dc3f7618e3b2df6f043a6a7e
RJ Hilario <rodjonh@gmail.com>
2024-03-18 17:39:45 +0800

    Changed description

--- bash/deploy-rhel8.sh
+++ bash/deploy-rhel8.sh
@@ -4,7 +4,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "ht
         "facility": "ny5",
         "plan": "c3.small.x86",
         "hostname": "rj-ny-rhel8",
-        "description": "RJ Deploy RHEL8 in TY",
+        "description": "RJ Deploy RHEL8 in NY",
         "operating_system": "rhel_8",
         "tags": ["RHEL8 Test"]
         }'

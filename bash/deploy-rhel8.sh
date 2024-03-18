273ce4b47d9593d49114846c2c8551aac64196d4
RJ Hilario <rodjonh@gmail.com>
2024-03-18 17:39:22 +0800

    Changed hostname

--- bash/deploy-rhel8.sh
+++ bash/deploy-rhel8.sh
@@ -3,7 +3,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "ht
     '{
         "facility": "ny5",
         "plan": "c3.small.x86",
-        "hostname": "rj-ty-rhel8",
+        "hostname": "rj-ny-rhel8",
         "description": "RJ Deploy RHEL8 in TY",
         "operating_system": "rhel_8",
         "tags": ["RHEL8 Test"]

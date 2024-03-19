2e3745bc0faf3d176d19c95ceaa2ab7ca5cda28b
RJ Hilario <rodjonh@gmail.com>
2024-03-20 05:43:48 +0800

    Changed facility to NY5

--- bash/deploy-server.sh
+++ bash/deploy-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" 'https://api.equinix.com/metal/v1/projects/${project_id}/devices' -d \
     '{
-        "facility": "hk4",
+        "facility": "ny5",
         "plan": "m3.small.x86",
         "hostname": "rj-hk-m3.small.x86",
         "description": "RJ Ubuntu 22.04 deployment in HK",

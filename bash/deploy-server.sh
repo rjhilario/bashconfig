a3b2f4db8a1f09b8f0d7954fb26c2d4f76c2743c
RJ Hilario <rodjonh@gmail.com>
2024-03-20 05:44:34 +0800

    Changed server config

--- bash/deploy-server.sh
+++ bash/deploy-server.sh
@@ -2,7 +2,7 @@
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" 'https://api.equinix.com/metal/v1/projects/${project_id}/devices' -d \
     '{
         "facility": "ny5",
-        "plan": "m3.small.x86",
+        "plan": "c3.small.x86",
         "hostname": "rj-hk-m3.small.x86",
         "description": "RJ Ubuntu 22.04 deployment in HK",
         "operating_system": "ubuntu_22_04",

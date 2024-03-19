df1155ec76eebffda00ba5c51222af5992ff1fd0
RJ Hilario <rodjonh@gmail.com>
2024-03-20 05:46:07 +0800

    Edited description

--- bash/deploy-server.sh
+++ bash/deploy-server.sh
@@ -4,7 +4,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" 'ht
         "facility": "ny5",
         "plan": "c3.small.x86",
         "hostname": "rj-ny-c3.small.x86",
-        "description": "RJ Ubuntu 22.04 deployment in HK",
+        "description": "RJ Ubuntu 22.04 deployment in NY",
         "operating_system": "ubuntu_22_04",
         "tags": ["Shell Script Deployment"],
         }'

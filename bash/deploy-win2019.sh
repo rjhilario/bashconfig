87eb654b2b8ea54d2ca863904041dffdd00259b3
RJ Hilario <rodjonh@gmail.com>
2024-03-22 22:48:42 +0800

    Edited hostname

--- bash/deploy-win2019.sh
+++ bash/deploy-win2019.sh
@@ -3,7 +3,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "ht
     '{
         "facility": "ny5",
         "plan": "c3.small.x86",
-        "hostname": "rj-hk-win2019",
+        "hostname": "rj-ny-win2019",
         "description": "RJ Windows Server 2019 Deployment in HK",
         "operating_system": "windows_2019",
         "tags": ["shell script deploy"]

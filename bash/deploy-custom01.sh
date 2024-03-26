7e79556d627e543b4311c41f0848edeb1b3435f3
RJ Hilario <rodjonh@gmail.com>
2024-03-26 22:25:45 +0800

    Changed server config

--- bash/deploy-custom01.sh
+++ bash/deploy-custom01.sh
@@ -2,7 +2,7 @@
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
         "facility": "ny5",
-        "plan": "m3.small.x86",
+        "plan": "c3.small.x86",
         "hostname": "rj-hk-custom-deploy",
         "description": "RJ Custom Deployment in HK",
         "operating_system": "custom_ipxe",

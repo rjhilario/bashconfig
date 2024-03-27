3ed18f80e219d049af9a61f121b8a041d4ff4e92
RJ Hilario <rodjonh@gmail.com>
2024-03-27 16:04:54 +0800

    Changed facility to NY5

--- bash/deploy-custom02.sh
+++ bash/deploy-custom02.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
-        "facility": "hk4",
+        "facility": "ny5",
         "plan": "m3.small.x86",
         "hostname": "rj-hk-custom-deploy",
         "description": "RJ Custom in HK",

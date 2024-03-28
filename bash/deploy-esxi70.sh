902cd29f3d6bde37281f1eaf71bc783171ea7e5e
RJ Hilario <rodjonh@gmail.com>
2024-03-28 21:32:12 +0800

    Changed server config to c3.medium.x86

--- bash/deploy-esxi70.sh
+++ bash/deploy-esxi70.sh
@@ -2,7 +2,7 @@
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" 'https://api.equinix.com/metal/v1/projects/${project_id}/devices' -d \
     '{
         "facility": "sv15",
-        "plan": "c3.small.x86",
+        "plan": "c3.medium.x86",
         "hostname": "rj-ny-esxi70",
         "description": "RJ ESXi 7.0 Deployment in NY",
         "operating_system": "vmware_esxi_7_0",

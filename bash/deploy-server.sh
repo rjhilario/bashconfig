e5eeb50fe1bbfa8dd43ecb4ad2c021e5d43045a5
RJ Hilario <rodjonh@gmail.com>
2024-03-30 07:36:16 +0800

    Edited hostname

--- bash/deploy-server.sh
+++ bash/deploy-server.sh
@@ -2,8 +2,8 @@
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" 'https://api.equinix.com/metal/v1/projects/${project_id}/devices' -d \
     '{
         "facility": "sv15",
-        "plan": "c3.small.x86",
-        "hostname": "rj-ny-c3.small.x86",
+        "plan": "c3.medium.x86",
+        "hostname": "rj-sv-c3.medium.x86",
         "description": "RJ Ubuntu 22.04 deployment in NY",
         "operating_system": "ubuntu_22_04",
         "tags": ["Shell Script Deployment"],

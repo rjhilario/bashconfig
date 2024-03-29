26b1fc6d4acc0157da17def925dc9c04821acbb6
RJ Hilario <rodjonh@gmail.com>
2024-03-29 18:00:12 +0800

    Edited facility

--- bash/deploy-rhel8.sh
+++ bash/deploy-rhel8.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
-        "facility": "ny5",
+        "facility": "sv15",
         "plan": "c3.small.x86",
         "hostname": "rj-ny-rhel8",
         "description": "RJ Deploy RHEL8 in NY",

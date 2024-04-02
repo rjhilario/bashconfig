d250cedd85151479bfd43de35ce55d0febf12b68
RJ Hilario <rodjonh@gmail.com>
2024-04-02 22:06:31 +0800

    Changed configuration

--- bash/deploy-win2016.sh
+++ bash/deploy-win2016.sh
@@ -3,7 +3,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "ht
     '{
         "facility": "sv15",
         "plan": "c3.small.x86",
-        "hostname": "rj-hk-win2016",
+        "hostname": "rj-sv-win2016",
         "description": "RJ Windows Server 2016 Deployment in HK",
         "operating_system": "windows_2016",
         "tags": ["shell script deploy"]

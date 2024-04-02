9d07c96c3f405131344c26f990623fb458e6b96c
RJ Hilario <rodjonh@gmail.com>
2024-04-02 22:05:43 +0800

    Edited configuration

--- bash/deploy-win2016.sh
+++ bash/deploy-win2016.sh
@@ -1,7 +1,7 @@
 #!/usr/bin
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
-        "facility": "ny5",
+        "facility": "sv15",
         "plan": "c3.small.x86",
         "hostname": "rj-hk-win2016",
         "description": "RJ Windows Server 2016 Deployment in HK",

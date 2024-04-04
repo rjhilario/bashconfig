d353811f61116d87dd4782da90bcdb94d33eb77e
RJ Hilario <rodjonh@gmail.com>
2024-04-04 14:28:02 +0800

    Edited configuration

--- bash/deploy-ubuntu20_04.sh
+++ bash/deploy-ubuntu20_04.sh
@@ -3,7 +3,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "ht
     '{
         "facility": "sv15",
         "plan": "c3.small.x86",
-        "hostname": "rj-ny-ubuntu20.04",
+        "hostname": "rj-sv-ubuntu20.04",
         "description": "RJ Ubuntu 20.04 deployment in NY",
         "operating_system": "ubuntu_20_04",
         "tags": ["shell script deploy"]

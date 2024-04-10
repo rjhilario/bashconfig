4c06850c925596b31aa9360471de388a6cf120fc
RJ Hilario <rodjonh@gmail.com>
2024-04-10 14:37:08 +0800

    Changed facility to AM6

--- bash/deploy-rhel8.sh
+++ bash/deploy-rhel8.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
-        "facility": "sv15",
+        "facility": "am6",
         "plan": "c3.small.x86",
         "hostname": "rj-sv-rhel8",
         "description": "RJ Deploy RHEL8 in SV",

a9e033dce89743857b2497790885723e0bb8f83d
RJ Hilario <rodjonh@gmail.com>
2024-04-11 19:13:56 +0800

    Changed facility to AM^

--- bash/deploy-rocky9.sh
+++ bash/deploy-rocky9.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
-        "facility": "sv15",
+        "facility": "am6",
         "plan": "c3.medium.x86",
         "hostname": "rj-sv-rocky9",
         "description": "RJ Rocky 9 Linux Deployment in SV",

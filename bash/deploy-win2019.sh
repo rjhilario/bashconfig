3954acf6b684f5570db1d3edd39df089f1cc77db
RJ Hilario <rodjonh@gmail.com>
2024-04-13 13:16:58 +0800

    Edited server config

--- bash/deploy-win2019.sh
+++ bash/deploy-win2019.sh
@@ -2,7 +2,7 @@
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
         "facility": "am6",
-        "plan": "c3.small.x86",
+        "plan": "c3.medium.x86",
         "hostname": "rj-sv-win2019",
         "description": "RJ Windows Server 2019 Deployment in SV",
         "operating_system": "windows_2019",

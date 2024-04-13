4a4a1d6de4ec0203c4c785342ce99b1662aed513
RJ Hilario <rodjonh@gmail.com>
2024-04-13 13:19:28 +0800

    Changed configuration

--- bash/deploy-n3.xlarge.x86.sh
+++ bash/deploy-n3.xlarge.x86.sh
@@ -4,7 +4,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "ht
         "facility": "am6",
         "plan": "n3.xlarge.x86",
         "hostname": "rj-am-n3.xlarge.x86",
-        "description": "RJ n3.xlarge.x86 deployment in HK",
+        "description": "RJ n3.xlarge.x86 deployment in AM",
         "operating_system": "ubuntu_22_04",
         "tags": ["shell script deploy"]
         }'

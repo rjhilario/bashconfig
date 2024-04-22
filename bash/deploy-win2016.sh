32c5b87dcec0a085d55145fc3b18d74a6af48966
rjhilario <rodjonh@gmail.com>
2024-04-22 23:43:35 +0800

    Changed server config

--- bash/deploy-win2016.sh
+++ bash/deploy-win2016.sh
@@ -2,7 +2,7 @@
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
         "facility": "ty11",
-        "plan": "c3.medium.x86",
+        "plan": "n3.xlarge.x86",
         "hostname": "rj-am-win2016",
         "description": "RJ Windows Server 2016 Deployment in AM",
         "operating_system": "windows_2016",

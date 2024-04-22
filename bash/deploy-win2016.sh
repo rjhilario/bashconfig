f237e1d8c28844db572d5894e7ca8b410315cbea
RJ Hilario <rodjonh@gmail.com>
2024-04-22 23:39:19 +0800

    Changed facility

--- bash/deploy-win2016.sh
+++ bash/deploy-win2016.sh
@@ -1,7 +1,7 @@
 #!/usr/bin
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
-        "facility": "am6",
+        "facility": "ty11",
         "plan": "c3.medium.x86",
         "hostname": "rj-am-win2016",
         "description": "RJ Windows Server 2016 Deployment in AM",

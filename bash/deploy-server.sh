e33ce97e15ee6286b72bf9b0d1d44ed4a5047f9a
rjhilario <rodjonh@gmail.com>
2024-04-29 21:46:22 +0800

    Changed hostname

--- bash/deploy-server.sh
+++ bash/deploy-server.sh
@@ -3,7 +3,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" 'ht
     '{
         "facility": "ty11",
         "plan": "c3.small.x86",
-        "hostname": "rj-am-c3.medium.x86-Ubuntu22",
+        "hostname": "rj-ty-c3.small.x86-Ubuntu22",
         "description": "RJ Ubuntu 22.04 deployment in AM",
         "operating_system": "ubuntu_22_04",
         "tags": ["Shell Script Deployment"],

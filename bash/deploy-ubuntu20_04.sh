6cd6fb46f597e23f1721568b9c2b1cb50609c89e
rjhilario <rodjonh@gmail.com>
2024-04-30 20:56:18 +0800

    Changed server config

--- bash/deploy-ubuntu20_04.sh
+++ bash/deploy-ubuntu20_04.sh
@@ -4,7 +4,7 @@ curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "ht
         "facility": "ty11",
         "plan": "c3.medium.x86",
         "hostname": "rj-ty-ubuntu20.04",
-        "description": "RJ Ubuntu 20.04 deployment in SV",
+        "description": "RJ Ubuntu 20.04 deployment in TY",
         "operating_system": "ubuntu_20_04",
         "tags": ["shell script deploy"]
         }'

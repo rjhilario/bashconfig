2cafe4dbe13e8ef89d1393dea6026ef9ca31a1f0
rjhilario <rodjonh@gmail.com>
2024-05-03 12:05:21 +0800

    Changed facility to TY11

--- bash/deploy-n3.xlarge.x86.sh
+++ bash/deploy-n3.xlarge.x86.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 curl -X POST -H 'Content-Type: application/json' -H "X-Auth-Token: $mytoken" "https://api.equinix.com/metal/v1/projects/${project_id}/devices" -d \
     '{
-        "facility": "am6",
+        "facility": "ty11",
         "plan": "n3.xlarge.x86",
         "hostname": "rj-am-n3.xlarge.x86",
         "description": "RJ n3.xlarge.x86 deployment in AM",
